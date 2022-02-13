3d74a7d877cb5e624d719b830670d6ad09769f47
AndreyCorpse280990 <280990corpse@gmail.com>
2022-02-13 09:35:26 +0300

    completed

---
+++ lists.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+# Создайте список из чисел 3, 5, 7, 9 и 10.5
+number = [3, 5, 7, 9, 10.5]
+# Выведите содержимое списка на экран
+print(number)
+# Добавьте в конец списка строку "Python"
+number.append('Python')
+# Выведите длину списка на экран
+print(len(number))
+
+# Выведите на экран начальный элемент списка
+print(number[0])
+# Выведите на экран последний элемент списка
+print(number[-1])
+# Напечатайте элементы списка со второго по четвертый включительно
+print(number[1:4])
+# удалите из списка строку "Python"
+del number[-1]
+
+# Создайте словарь
+dictionary = {"city": "Москва", "temperature": "20"}
+# Выведите на экран значение ключа city
+print(dictionary['city'])
+# Уменьшите значение "temperature" на 5
+dictionary['temperature'] = 5
+# Выведите на экран весь словарь
+print(dictionary)
+
+# Проверьте, есть ли в словаре ключ country
+print(dictionary.get('country'))
+# Выведите значение по-умолчанию "Россия" для ключа country
+print(dictionary.get('country', 'Россия'))
+# Добавьте в словарь элемент date со значением "27.05.2019"
+dictionary['date'] = '27.05.2019'
+# Выведите на экран длину словаря
+print(len(dictionary))
